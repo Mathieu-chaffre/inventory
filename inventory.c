77006a259eeaa0ff1116d655f773e26d81a73ead
Mathieu-chaffre <chaffrem@gmail.com>
2020-01-27 12:51:46 +0100

    update

--- inventory.c
+++ inventory.c
@@ -23,12 +23,13 @@ marchandise_a_vendre poison = {"poison",10,30};
 int demande;
 int nombre;
 int argent = 500;
+int achat=1;
 
 void random(){
   srand(time(NULL));
-  viande.nombre =rand()%10+1;
+  viande.nombre =rand()%100+1;
   srand(time(NULL));
-  epee.nombre =rand()%5+1;
+  epee.nombre =rand()%30+1;
   srand(time(NULL));
   fleches.nombre =rand()%50+1;
 }
@@ -36,28 +37,48 @@ void random(){
 
 int main(){
   random();
+  while (achat ==1) {
   printf("Que veux tu acheter au marchands ? Il lui reste %d %s au prix de %d pieces , %d %s au prix de %d pieces, %d %s au prix de %d pieces, %d %s au prix de %d pieces. \n", viande.nombre, viande.nom,viande.prix, epee.nombre, epee.nom,epee.prix, fleches.nombre, fleches.nom, fleches.prix, poison.nombre, poison.nom,poison.prix );
-  scanf("%d\n", demande);
+  scanf("%d", &demande);
   if (demande == 1) {
-    scanf("%d\n", nombre);
+    printf("Tu veux en acheter combien ?\n");
+    scanf("%d", &nombre);
+    if (nombre > viande.nombre) {
+      printf("Le marchand n'en a pas autant\n");
+    }
+    else{
     viande.nombre = viande.nombre - nombre;
     printf("Tu achetes %d morceau de viande\n", nombre);
-    argent = argent - viande.prix;
+    argent = argent - (viande.prix*nombre);
     printf("Il te reste que %d pieces\n", argent);
   }
+}
   if (demande == 2) {
-    scanf("%d\n", nombre);
+    printf("Tu veux en acheter combien ?\n");
+    scanf("%d", &nombre);
     epee.nombre = epee.nombre - nombre;
     printf("Tu achetes %d epees\n", nombre);
-    argent = argent - epee.prix;
+    argent = argent - (epee.prix*nombre);
     printf("Il te reste que %d pieces\n", argent);
   }
   if (demande == 3) {
-    scanf("%d\n", nombre );
+    printf("Tu veux en acheter combien ?\n");
+    scanf("%d", &nombre );
     fleches.nombre = fleches.nombre - nombre;
     printf("Tu achete %d fleches\n", nombre);
-    argent = argent - fleches.prix;
+    argent = argent - (fleches.prix*nombre);
     printf("Il te reste que %d pieces\n", argent);
   }
+  if (demande == 4) {
+    printf("Tu veux en acheter combien ?\n");
+    scanf("%d", nombre);
+    poison.nombre = poison.nombre - nombre;
+    printf("Tu as donc acheter %d poisons\n",nombre );
+    argent = argent - (poison.prix*nombre);
+  }
+  if (argent <=0) {
+    achat = 0;
+  }
+}
   return 0;
 }
